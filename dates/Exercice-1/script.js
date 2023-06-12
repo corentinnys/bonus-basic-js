12d5901d5665b44a7ece3c8e991aa13f761994a8
corentin Nys <corentinys@gmail.com>
2023-06-12 16:00:44 +0200

    Dates - exercice 1

---
+++ dates/Exercice-1/script.js
@@ -0,0 +1,32 @@
+/* becode/javascript
+ *
+ * /04-dates/01-title-by-hour-one/script.js - 4.1: texte en fonction de l'heure (1)
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function() {
+
+    // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
+
+    // your code here
+    let paragraph =document.querySelector('p')
+    document.querySelector('#target').onclick = function () {
+        let date = new Date()
+        let hour =  date.getHours();
+        if (hour<18){
+            paragraph.innerHTML = "hello";
+
+        }else{
+            paragraph.innerHTML = "good evening";
+        }
+
+    }
+
+
+
+})();
