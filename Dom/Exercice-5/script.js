1fe040e4cd09d6edbd73f6b9d3fde5add9a0ad11
corentin Nys <corentinys@gmail.com>
2023-06-13 20:11:58 +0200

    Dom - exercice 5

---
+++ Dom/Exercice-5/script.js
@@ -0,0 +1,21 @@
+/* becode/javascript
+ *
+ * /06-dom/05-hover-image/script.js - 6.5: survol d'image
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function() {
+
+    // your code here
+    document.querySelector('img').onmouseover = (event)=>
+    {
+        let dataHover =event.currentTarget.getAttribute('data-hover');
+        event.currentTarget.src = dataHover;
+    }
+
+})();
