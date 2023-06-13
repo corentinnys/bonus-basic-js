6ad80d041445488829b5600865f56f2556184189
corentin Nys <corentinys@gmail.com>
2023-06-13 09:52:59 +0200

    Array - exercice 8

---
+++ array/Exercice-8/script.js
@@ -0,0 +1,39 @@
+/* becode/javascript
+ *
+ * /05-arrays/08-array-includes/script.js - 5.8: présence dans un tableau
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function() {
+
+    const fruits = [
+        "apple",
+        "perry",
+        "strawberry",
+        "tomato",
+        "kiwi",
+        "banana",
+        "orange",
+        "mandarin",
+        "durian",
+        "peach",
+        "grapes",
+        "cherry",
+    ];
+
+    // your code here
+    document.querySelector('#run').onclick = function () {
+        if (fruits.includes('Pommes')== true)
+        {
+            console.log('il y a des pommes');
+        }else{
+            console.log('pas de pommes');
+        }
+    }
+
+})();
